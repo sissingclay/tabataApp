514920733b0a40abfe999aa1925fd300d62524ce
Clay Sissing <claysissing@gmail.com>
2014-07-26 22:16:08 +0100

    my first commit

---
+++ www/js/route.js
@@ -0,0 +1,18 @@
+/**
+ * Created by clay on 26/07/14.
+ */
+tabataApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'view/home.html',
+                controller: ''
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
